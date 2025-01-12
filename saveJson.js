088bda4ee52b8ce01473265ac4652e55dfee582e
Niayesh99 <n.tabrikk@gmail.com>
2025-01-12 15:58:22 +0100

    create and export saveJson function

--- saveJson.js
+++ saveJson.js
@@ -1,25 +1,12 @@
-import { Task } from "./task.js";
 import fs from "fs";
-import ToDoList from "./ToDo.js";
+export default function saveJson(content, fileName) {
+  const jsonString = JSON.stringify(content, null, 2);
 
-const toDos = new ToDoList();
-const myTask = new Task("learn x", "25.02", "pending");
-const myTask1 = new Task("learn y", "25.03", "pending");
-const myTask2 = new Task("learn a", "25.04", "pending");
-const myTask3 = new Task("learn b", "25.05", "pending");
-
-toDos.addNewTask(myTask);
-toDos.addNewTask(myTask1);
-toDos.addNewTask(myTask2);
-toDos.addNewTask(myTask3);
-
-const jsonString = JSON.stringify(toDos, null, 2);
-
-fs.writeFile("tasks.json", jsonString, (err) => {
-  if (err) {
-    console.log("Error writing file", err);
-  } else {
-    console.log("File saved successfully as tasks.json");
-  }
-});
-
+  fs.writeFile(fileName, jsonString, (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("File saved successfully as "+ fileName);
+    }
+  });
+}
