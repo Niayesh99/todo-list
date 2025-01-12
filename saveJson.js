b56bdf565318ed213d0a291f1411e732399072ae
Niayesh99 <n.tabrikk@gmail.com>
2025-01-12 15:00:28 +0100

    Import fs to save object to json file

--- saveJson.js
+++ saveJson.js
@@ -1,15 +1,18 @@
-const fs = require('fs');
-const myTask = {
-    title: 'learn Deutsch',
-    dueDate: "25.01",
-    status: "pending"
-};
-const jsonString = JSON.stringify(myTask, null, 2); 
+import { Task } from "./task.js";
+import fs from "fs";
+const myTask = new Task("learn x", "25.02", "pending");
+const myTask1 = new Task("learn y", "25.03", "pending");
+const myTask2 = new Task("learn a", "25.04", "pending");
+const myTask3 = new Task("learn b", "25.05", "pending");
 
-fs.writeFile('tasks.json', jsonString, (err) => {
-    if (err) {
-        console.log('Error writing file', err);
-    } else {
-        console.log('File saved successfully as tasks.json');
-    }
+const jsonString = JSON.stringify(myTask, null, 2);
+
+
+fs.writeFile("tasks2.json", jsonString, (err) => {
+  if (err) {
+    console.log("Error writing file", err);
+  } else {
+    console.log("File saved successfully as tasks.json");
+  }
 });
+
