879b97d486338a3c7e563e3b9f0d0a4184beffba
Niayesh99 <n.tabrikk@gmail.com>
2025-01-12 16:01:50 +0100

    import and use saveJson function to save toDos

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Task } from "./task.js";
+import fs from "fs";
+import ToDoList from "./ToDo.js";
+import saveJson from "./saveJson.js";
+
+const toDos = new ToDoList();
+const myTask = new Task("learn x", "25.02", "pending");
+const myTask1 = new Task("learn y", "25.03", "pending");
+const myTask2 = new Task("learn a", "25.04", "pending");
+const myTask3 = new Task("learn b", "25.05", "pending");
+
+toDos.addNewTask(myTask);
+toDos.addNewTask(myTask1);
+toDos.addNewTask(myTask2);
+toDos.addNewTask(myTask3);
+saveJson(toDos, "tasks.json");
