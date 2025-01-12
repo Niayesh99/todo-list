906d2897514061386f469d59def69bafcf291040
Niayesh99 <n.tabrikk@gmail.com>
2025-01-12 15:40:44 +0100

    create a new ToDoList

--- saveJson.js
+++ saveJson.js
@@ -1,6 +1,8 @@
 import { Task } from "./task.js";
 import fs from "fs";
 import ToDoList from "./ToDo.js";
+
+const toDos = new ToDoList();
 const myTask = new Task("learn x", "25.02", "pending");
 const myTask1 = new Task("learn y", "25.03", "pending");
 const myTask2 = new Task("learn a", "25.04", "pending");
